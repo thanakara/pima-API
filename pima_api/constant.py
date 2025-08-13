4efc5311b17faeebb512b52bc344fadfd847ea6f
pima-API <demo@example.com>
2025-08-13 17:13:04 +0300

    Data preprocessing && Training Job fix
    + Used NaN values for the zero instances and median after
    + Added an xgb=False argument to make a new model
    + Job artifacts are now model.pkl and report.json

--- pima_api/constant.py
+++ pima_api/constant.py
@@ -6,4 +6,5 @@ class Filepath(Enum):
     ROOT = Path.cwd()
     DATAPATH = ROOT / "pima_api" / "data" / "pima.csv"
     CONFIGPATH = ROOT / "pima_api" / "conf"
-    MODELJOB = ROOT / "pima_api" / "model" / "model.joblib"
+    REPORTPATH = ROOT / "pima_api" / "model" / "report.json"
+    MODELJOB = REPORTPATH.with_name("model.pkl")
--- pima_api/model/job.py
+++ pima_api/model/job.py
@@ -2,16 +2,15 @@ import json
 from pathlib import Path
 
 import joblib
-import matplotlib.pyplot as plt
 from omegaconf import DictConfig
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import ConfusionMatrixDisplay, classification_report
+from sklearn.metrics import classification_report
 
 from pima_api.constant import Filepath
 from pima_api.data.preprocess import stratify_split_dataset
 
 
-def fit_report_and_serialize(config: DictConfig) -> RandomForestClassifier:
+def randf_fit_report_and_serialize(config: DictConfig) -> RandomForestClassifier:
     rand_f = RandomForestClassifier(
         n_estimators=config.n_estimators,
         criterion=config.criterion,
@@ -32,4 +31,17 @@ def fit_report_and_serialize(config: DictConfig) -> RandomForestClassifier:
     with Path.open(Filepath.MODELJOB.value, "wb") as f_:
         joblib.dump(rand_f, f_, protocol=4)
 
+    y_pred = rand_f.predict(X_test)
+    report = classification_report(
+        y_test, y_pred, target_names=["Negative", "Positive"], output_dict=True
+    )
+    report_json = json.dumps(report, indent=4)
+
+    with Path.open(Filepath.REPORTPATH.value, "w") as f_:
+        f_.write(report_json)
+
     return rand_f
+
+
+def xbg_fit_report_and_serialize(config: DictConfig):
+    raise NotImplementedError
